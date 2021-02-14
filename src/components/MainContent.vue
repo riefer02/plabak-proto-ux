04deb4ea9722dad9d391f4de44853d16da5d1e1a
riefer02 <andrew.riefenstahl@gmail.com>
2021-02-13 20:52:27 -0600

    logo placeholder updated

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="flex h-screen justify-center items-center text-center">
-    <img src="../assets/images/logo-1.png" class="h-60 relative bottom-40" />
+    <img
+      src="../assets/images/logo-1.png"
+      class="h-60 shadow-lg rounded-lg relative bottom-40 bg-gray-100"
+    />
   </div>
 </template>
 
